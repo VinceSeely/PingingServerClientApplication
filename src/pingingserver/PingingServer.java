299b68d3f7cc9a4777d36bf048e089b40b7c4be9
Raj Kumar Lama <lamar@uwplatt.edu>
2016-12-03 17:57:46 -0600

    missed the file

--- src/pingingserver/PingingServer.java
+++ src/pingingserver/PingingServer.java
@@ -9,13 +9,46 @@ package pingingserver;
  *
  * @author seelyv rajlamar
  */
+import java.net.ServerSocket;
+import java.net.Socket;
 public class PingingServer {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    ServerSocket serverSocket;
+    private final static int PORT_NUMBER = 5777;
+
+    public static void main(String[] args) 
+    {
+        try
+        {
+          PingingServer server = new PingingServer ();    
+          server.run();
+ 
+        }
+        catch(Exception ex)
+        {
+           System.out.println("Error: " + ex);
+        }
+        
+    }
+    public void run()
+    {
+        try
+        {
+            ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+            while(true)
+            {
+               Socket socket = serverSocket.accept();
+               PingingThread ping = new PingingThread(socket);
+               ping.start();
+            }
+        }
+        catch(Exception ex)    
+        {
+           System.out.println("Error: " + ex);
+        }
     }
     
 }
